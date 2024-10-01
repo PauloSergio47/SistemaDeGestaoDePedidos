c134cf38e2820104df499918fad49a2ceb5f9fb8
Paulo Sergio <paulosmatos015@gmail.com>
2024-10-01 19:26:03 -0300

    Criando o versionamento git

---
+++ src/test/java/com/paulosergio/SistemaDeGestaoDePedidos/SistemaDeGestaoDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulosergio.SistemaDeGestaoDePedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
